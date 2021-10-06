d9369f453de98a82a4fe0ebdf6b52759a918fd9e
pepijnseigers <p.seigers@emendis.nl>
2021-10-06 08:29:12 +0200

    feat: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { DxDiagramComponent } from 'devextreme-angular';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'poc-devextreme';
+  @ViewChild(DxDiagramComponent)
+  public diagramComponent: DxDiagramComponent;
+  public selectedElement: any;
+  showSidebar: boolean;
+  diagramConfig: any[] = [];
+
+  public configFormGroup: FormGroup;
+
+  /** shapeConfig
+   * {id: 'shapeId', ...properties, type}
+   */
+
+  constructor(private formBuilder: FormBuilder) {
+    this.configFormGroup = this.formBuilder.group({
+      id: [],
+      name: [],
+    });
+  }
+
+  change(ev): void {
+    if (ev.items.length) {
+      this.showSidebar = true;
+      this.selectedElement = ev.items[ev.items.length - 1];
+
+      console.log(this.selectedElement);
+    }
+  }
+
+  save(): void {
+    console.log(JSON.parse(this.diagramComponent.instance.export()));
+  }
+
+  public submit(): void {
+    this.diagramConfig.push({
+      name: this.configFormGroup.value.name,
+      id: this.selectedElement.id,
+      type: this.selectedElement.type,
+    });
+
+    this.showSidebar = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DxDiagramModule } from 'devextreme-angular';
+import { SidebarModule } from 'primeng/sidebar';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DxDiagramModule,
+    SidebarModule,
+    ReactiveFormsModule,
+    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
